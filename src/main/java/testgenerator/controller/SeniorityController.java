5597ee02b2dfc82a321601b3772a798e87b694a6
Keti <ketevan.bardakovi@gmail.com>
2023-01-13 13:06:28 +0400

    testing seniority branch

--- src/main/java/testgenerator/controller/SeniorityController.java
+++ src/main/java/testgenerator/controller/SeniorityController.java
@@ -1,7 +1,11 @@
 package testgenerator.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/seniority")
+@RequiredArgsConstructor
 public class SeniorityController {
 }
