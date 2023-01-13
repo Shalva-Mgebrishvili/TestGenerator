99bd843e41b9e1af21d81c175857cd30bfb700c1
Shako Mgebrishvili <shalva.mgebrishvili.1@gmail.com>
2023-01-13 14:45:02 +0400

    Stack - controller+facade+service+repo+mapper+param

--- src/main/java/testgenerator/controller/StackController.java
+++ src/main/java/testgenerator/controller/StackController.java
@@ -1,11 +1,54 @@
 package testgenerator.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import testgenerator.facade.StackFacade;
+import testgenerator.model.dto.StackDto;
+import testgenerator.model.params.StackParam;
 
 @RestController
 @RequestMapping("/stacks")
 @RequiredArgsConstructor
 public class StackController {
+
+    private final StackFacade facade;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StackDto> findById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(facade.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<StackDto>> findAll(
+            @RequestParam(value = "page", defaultValue = "0", required = false) Integer page,
+            @RequestParam(value = "size", defaultValue = "20", required = false) Integer size,
+            @RequestParam(value = "direction", defaultValue = "ASC", required = false) Sort.Direction direction,
+            @RequestParam(value = "sort", defaultValue = "id", required = false) String sort){
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sort));
+
+        return ResponseEntity.status(HttpStatus.OK).body(facade.findAll(pageable));
+    }
+
+    @PostMapping
+    public ResponseEntity<StackDto> add(@RequestBody StackParam param) {
+        return ResponseEntity.status(HttpStatus.OK).body(facade.add(param));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StackDto> update(@PathVariable Long id, @RequestBody StackParam param) {
+        return ResponseEntity.status(HttpStatus.OK).body(facade.update(id, param));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        facade.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
