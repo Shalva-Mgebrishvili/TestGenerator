5171bc66b8572128bb993d989460029427f641b5
Shako Mgebrishvili <shalva.mgebrishvili.1@gmail.com>
2023-01-17 19:05:28 +0400

    Swagger+WebSecurity

---
+++ src/main/java/testgenerator/config/WebSecurity.java
@@ -0,0 +1,25 @@
+package testgenerator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+//@EnableGlobalMethodSecurity
+public class WebSecurity {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll();
+
+        return http.build();
+    }
+}
